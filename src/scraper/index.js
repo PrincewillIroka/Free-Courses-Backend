2011f395517bc723f1686f869aff5cc76bb72a41
Princewill Iroka <princewilliroka@outlook.com>
2020-10-30 17:05:44 +0100

    Updated dependencies

--- src/scraper/index.js
+++ src/scraper/index.js
@@ -1,5 +1,11 @@
 import playwright, { chromium } from "playwright";
 
+const sites = [
+  { name: "udemy", url: "https://www.udemy.com/" },
+  { name: "openclassrooms", url: "https://openclassrooms.com/" },
+  { name: "coursera", url: "https://www.coursera.org/" },
+];
+
 const scraper = async () => {
   const browser = await chromium.launch({ headless: false });
   const context = await browser.newContext({
@@ -7,12 +13,12 @@ const scraper = async () => {
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36",
   });
   const page = await context.newPage();
-  await page.goto("https://www.udemy.com/");
-  await page.waitForSelector("#header-search-field");
-  await page.fill("#header-search-field", "free courses");
-  await page.press('#header-search-field', 'Enter');
-  await page.screenshot({ path: `example-chromium.png` });
-  await browser.close();
+  await page.goto(sites[0].url);
+  await page.waitForLoadState({ waitUntil: "domcontentloaded" });
+  await page.fill('input[placeholder="Search for anything"]', "free courses");
+  await page.press('input[placeholder="Search for anything"]', "Enter");
+  // await page.screenshot({ path: `example-chromium.png` });
+  // await browser.close();
 };
 
 export default scraper;
